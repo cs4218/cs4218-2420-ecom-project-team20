63373bd942918017d6b950427098fb8b2dd781f5
Jason Ray <jasonraymoniaga@gmail.com>
2025-02-20 23:55:14 +0800

    Test updateCategoryController.js

--- controllers/categoryController.test.js
+++ controllers/categoryController.test.js
@@ -1,8 +1,9 @@
 import { expect, jest } from "@jest/globals";
-import { categoryController, createCategoryController } from "./categoryController.js";
+import { categoryController, createCategoryController, updateCategoryController } from "./categoryController.js";
 import categoryModel from "../models/categoryModel.js";
 import mongoose from "mongoose";
 import slugify from "slugify";
+import { error } from "console";
 
 jest.mock("../models/categoryModel.js", () => {
   const mockConstructor = jest.fn().mockImplementation(function (data) {
@@ -12,6 +13,7 @@ jest.mock("../models/categoryModel.js", () => {
     };
   });
   mockConstructor.findOne = jest.fn();
+  mockConstructor.findByIdAndUpdate = jest.fn();
   mockConstructor.create = jest.fn();
   mockConstructor.save = jest.fn();
   return mockConstructor;
@@ -27,6 +29,7 @@ jest.mock("mongoose", () => ({
 
 describe("Create Category Controller Test", () => {
   let req, res;
+  const categoryName = "testCategory"
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -34,8 +37,8 @@ describe("Create Category Controller Test", () => {
 
     req = {
       body: {
-        name: "testCategory",
-        slug: slugify("testCategory"),
+        name: categoryName,
+        slug: slugify(categoryName),
       }
     }
 
@@ -59,5 +62,116 @@ describe("Create Category Controller Test", () => {
     expect(categoryModel.findOne).toHaveBeenCalledWith({ name: req.body.name });
     expect(categoryModel.save).not.toHaveBeenCalled();
   });
-})
+
+  test("create new category will add it to categories", async () => {
+    categoryModel.findOne.mockResolvedValue(null);
+
+    categoryModel.create.mockResolvedValue({
+      name: req.body.name,
+      slug: req.body.slug,
+    })
+
+    await createCategoryController(req, res);
+    
+    expect(categoryModel.findOne).toHaveBeenCalledWith({ name: req.body.name });
+    expect(categoryModel).toHaveBeenCalledWith(
+      expect.objectContaining({
+        name: req.body.name,
+        slug: req.body.slug,
+      })
+    )
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.send).toHaveBeenCalledWith({
+      success: true,
+      message: "new category created",
+      category: {
+        name: req.body.name,
+        slug: req.body.slug,
+      }
+    })
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.useRealTimers();
+  });
+
+  afterAll(() => {
+    jest.resetModules();
+  });
+
+});
+
+describe("Update Category Controller Test", () => {
+  const categoryOld = "categoryOld";
+  const categoryNew = "categoryNew";
+  const validId = "validId";
+  const invalidId = "invalidId";
+  let req, res;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.useFakeTimers();
+
+    req = {
+      params: { id: validId },
+      body: {
+        name: categoryNew,
+        slug: slugify(categoryNew),
+      },
+    };
+
+    res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn().mockReturnThis(),
+      json: jest.fn().mockReturnThis(),
+    };
+  });
+
+  test("Update category with valid id", async () => {
+    categoryModel.findByIdAndUpdate.mockResolvedValue(validId, {name: req.body.name, slug: req.body.slug}, {new: true});
+
+    await updateCategoryController(req, res);
+
+    expect(categoryModel.findByIdAndUpdate).toHaveBeenCalledWith(
+      req.params.id,
+      { name: req.body.name, slug: req.body.slug },
+      { new: true }
+    );
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.send).toHaveBeenCalledWith({
+      success: true,
+      messsage: "Category Updated Successfully",
+      category: validId,
+    });
+  });
+
+  test("Update category with invalid id", async () => {
+    req.params.id = invalidId;
+
+    await updateCategoryController(req, res);
+
+    expect(categoryModel.findByIdAndUpdate).toHaveBeenCalledWith(
+      invalidId,
+      { name: req.body.name, slug: req.body.slug },
+      { new: true }
+    );
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith({
+      success: false,
+      message: "Error while updating category"
+    });
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.useRealTimers();
+  });
+
+  afterAll(() => {
+    jest.resetModules();
+  });
+});
+
+
 
