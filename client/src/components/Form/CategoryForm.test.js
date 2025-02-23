681d133d27cf1a94591899abe6d1958422ae76db
Kalaris Ng <kalaris3255@gmail.com>
2025-02-24 01:07:04 +0800

    test: add test to check rendering of CategoryForm component

--- client/src/components/Form/CategoryForm.test.js
+++ client/src/components/Form/CategoryForm.test.js
@@ -2,8 +2,31 @@ import { render, fireEvent } from "@testing-library/react";
 import { useState } from "react";
 import CategoryForm from "./CategoryForm";
 import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
 
 describe("CategoryForm Component", () => {
+  let handleSubmit, setValue;
+  beforeEach(() => {
+    handleSubmit = jest.fn(); // Mock submit function
+    setValue = jest.fn(); // Mock setValue function
+  });
+
+  it("renders input and submit button", () => {
+    const { getByPlaceholderText, getByRole } = render(
+      <MemoryRouter>
+        <CategoryForm
+          handleSubmit={handleSubmit}
+          value=""
+          setValue={setValue}
+        />
+      </MemoryRouter>
+    );
+
+    expect(getByPlaceholderText("Enter new category")).toBeInTheDocument();
+    expect(getByRole("button", { name: /submit/i })).toBeInTheDocument();
+  });
+
   it("clears input field after submission", () => {
     const MockComponent = () => {
       const [value, setValue] = useState(""); // Local state for input
