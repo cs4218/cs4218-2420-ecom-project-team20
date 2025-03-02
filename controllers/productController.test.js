182bfae57480093a3ec0e82c5ffc95501b2f1837
Jason Ray <jasonraymoniaga@gmail.com>
2025-03-02 14:53:40 +0800

    Encounter problems with mocking fs module

--- controllers/productController.test.js
+++ controllers/productController.test.js
@@ -1,12 +1,13 @@
-import { beforeEach, describe, expect, jest } from "@jest/globals";
 import {
   createProductController,
   updateProductController,
   deleteProductController,
 } from "./productController.js";
 import productModel from "../models/productModel.js";
-import mongoose, { Types } from "mongoose";
+import mongoose from "mongoose";
 import slugify from "slugify";
+import fs from "fs"
+import braintree from "braintree";
 
 jest.mock("../models/productModel.js", () => {
   const mockConstructor = jest.fn().mockImplementation(function (data) {
@@ -22,61 +23,106 @@ jest.mock("../models/productModel.js", () => {
   mockConstructor.save = jest.fn();
   return mockConstructor;
 });
+jest.mock("slugify");
+jest.mock("fs", () => {
+    readFileSync = jest.fn();
+})
 
 jest.mock("mongoose", () => ({
   models: {},
   model: jest.fn(),
   Schema: jest.fn(),
   Types: {
-    ObjectId: jest.fn(() => 'mock-object-id'), // Mock ObjectId to return a fixed value
+    ObjectId: jest.fn(() => "mock-object-id"), // Mock ObjectId to return a fixed value
   },
   connect: jest.fn(),
   connection: { close: jest.fn() },
 }));
 
-describe("Create Product Controller Test", () => {
+describe("Product Controller Test", () => {
   let req, res;
-  const productName = "productName";
-  const productDescription = "This is the product description";
-  const productPrice = 99.99;
-  const productCategory = mongoose.Types.ObjectId();
-  const productQuantity = 10;
-  const productPhoto = "productPhoto";
-  const productShipping = true;
 
   beforeEach(() => {
     jest.clearAllMocks();
     jest.useFakeTimers();
 
     req = {
-      fields: {
-        name: productName,
-        description: productDescription,
-        price: productPrice,
-        category: productCategory,
-        quantity: productQuantity,
-        shipping: productShipping,
-      },
-      files: {
-        photo: productPhoto,
-      },
+      fields: {},
+      files: {},
     };
 
     res = {
       status: jest.fn().mockReturnThis(),
       send: jest.fn().mockReturnThis(),
-      json: jest.fn().mockReturnThis(),
     };
+
+    slugify.mockImplementation((str) => `${str}-slug`);
   });
 
-  test("should successfully create a new product", async () => {
-    productModel.findOne.mockResolvedValue(null);
+  describe("createProductController", () => {
+    it("should successfully create a new product", async () => {
+      req.fields = {
+        name: "Test Product",
+        description: "This is description of test product",
+        price: 99.99,
+        category: mongoose.Types.ObjectId(),
+        quantity: 10,
+        shipping: true,
+      };
+      req.files = {
+        photo: {
+          size: 300000,
+          path: "../client/public/photo.jpg",
+        },
+      };
+      const mockProduct = { ...req.fields, slug: slugify("Test Product") };
 
-    const mockProductModel = { ...req.fields, slug: slugify(req.fields.name) }
-    productModel.create.mockResolvedValue();
+      productModel.findOne.mockResolvedValue(null);
+      productModel.mockImplementation(() => ({
+        save: jest.fn().mockResolvedValue(mockProduct),
+      }));
+
+      await createProductController(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(201);
+      expect(res.send).toHaveBeenCalledWith({
+        success: true,
+        message: "Product Created Successfully",
+        mockProduct,
+      });
+    });
+
+    it("should handle errors", async () => {
+      req.fields = {
+        name: "Test Product",
+        description: "This is description of test product",
+        price: 99.99,
+        category: mongoose.Types.ObjectId(),
+        quantity: 10,
+        shipping: true,
+      };
+      req.files = {
+        photo: {
+          size: 300000,
+          path: "../client/public/photo.jpg",
+        },
+      };
+      const mockError = new Error("Database Error");
+      const spy = jest.spyOn(console, "log");
+
+      productModel.findOne.mockRejectedValue(mockError);
+
+      await createProductController(req, res);
+
+      expect(spy).toHaveBeenCalledWith(mockError);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.send).toHaveBeenCalledWith({
+        success: false,
+        error: mockError,
+        message: "Error in crearing product",
+      });
+
+      spy.mockRestore();
+    });
   });
 });
-
-describe("Update Product Controller Test", () => {});
-
-describe("Delete Product Controller Test", () => {});
