b983bfb3b1a8a19c42437a3fd9142b610af02aaf
cetigerlily <celestigerlily@gmail.com>
2025-03-04 22:47:11 +0800

    Test Users

---
+++ client/src/pages/admin/Users.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import "@testing-library/jest-dom/extend-expect";
+
+import Users from "./Users";
+
+const testUser = {
+  name: "John Smith",
+  email: "johnsmith@email.com",
+  address: "10 Apple Street"
+};
+
+jest.mock("../../context/auth", () => ({
+  useAuth: jest.fn(() => [{
+    user: testUser
+  }, jest.fn()]),
+}));
+jest.mock("../../context/cart", () => ({
+  useCart: jest.fn(() => [null, jest.fn()]),
+}));
+jest.mock("../../context/search", () => ({
+  useSearch: jest.fn(() => [{ keyword: "" }, jest.fn()]),
+}));
+
+describe("Users", () => {
+  it('renders heading', () => {
+    render(
+      <MemoryRouter>
+        <Users />
+      </MemoryRouter>
+    );
+    expect(screen.getByText("All Users")).toBeInTheDocument();
+  });
+});
