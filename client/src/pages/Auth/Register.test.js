6a3fc28b87300e41fcdbaebf0ea52c4262e3199a
Kalaris Ng <kalaris3255@gmail.com>
2025-03-08 00:24:30 +0800

    fix: fix mocks for register.js

--- client/src/pages/Auth/Register.test.js
+++ client/src/pages/Auth/Register.test.js
@@ -6,22 +6,21 @@ import "@testing-library/jest-dom/extend-expect";
 import toast from "react-hot-toast";
 import Register from "./Register";
 
-// Mocking axios.post
 jest.mock("axios");
 jest.mock("react-hot-toast");
 
 jest.mock("../../context/auth", () => ({
-  useAuth: jest.fn(() => [null, jest.fn()]), // Mock useAuth hook to return null state and a mock function for setAuth
+  useAuth: jest.fn().mockReturnValue([null, jest.fn()]),
 }));
 
 jest.mock("../../context/cart", () => ({
-  useCart: jest.fn(() => [null, jest.fn()]), // Mock useCart hook to return null state and a mock function
+  useCart: jest.fn().mockReturnValue([null, jest.fn()]),
 }));
 
-jest.mock("../../hooks/useCategory", () => jest.fn(() => []));
+jest.mock("../../hooks/useCategory", () => jest.fn().mockReturnValue([]));
 
 jest.mock("../../context/search", () => ({
-  useSearch: jest.fn(() => [{ keyword: "" }, jest.fn()]), // Mock useSearch hook to return null state and a mock function
+  useSearch: jest.fn().mockReturnValue([{ keyword: "" }, jest.fn()]),
 }));
 
 describe("Register Component", () => {
