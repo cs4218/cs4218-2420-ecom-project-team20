eee6864d80684331f798036ac6d3ef07becf576d
Kalaris Ng <kalaris3255@gmail.com>
2025-03-08 13:56:51 +0800

    fix: fix tests for categoryform

--- client/src/components/Form/CategoryForm.test.js
+++ client/src/components/Form/CategoryForm.test.js
@@ -1,11 +1,8 @@
 import { render, fireEvent, waitFor } from "@testing-library/react";
-import { useState } from "react";
 import CategoryForm from "./CategoryForm";
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import "@testing-library/jest-dom";
-import toast from "react-hot-toast";
-import axios from "axios";
 
 // Ensure axios is fully mocked
 jest.mock("axios");
@@ -17,11 +14,11 @@ jest.mock("react-hot-toast", () => ({
 describe("CategoryForm Component", () => {
   let handleSubmit, setValue;
   beforeEach(() => {
-    handleSubmit = jest.fn(); // Mock submit function
-    setValue = jest.fn(); // Mock setValue function
+    handleSubmit = jest.fn();
+    setValue = jest.fn();
   });
 
-  it("renders input field and submit button", () => {
+  it("renders CategoryForm component correctly", () => {
     const { getByPlaceholderText, getByRole } = render(
       <MemoryRouter>
         <CategoryForm
@@ -36,7 +33,7 @@ describe("CategoryForm Component", () => {
     expect(getByRole("button", { name: /submit/i })).toBeInTheDocument();
   });
 
-  it("allows user to type in input field", () => {
+  it("allows user to type in input field and updates input value  on change", () => {
     const { getByPlaceholderText } = render(
       <MemoryRouter>
         <CategoryForm
@@ -57,21 +54,24 @@ describe("CategoryForm Component", () => {
     const handleSubmitMock = jest.fn();
     const setValueMock = jest.fn();
 
-    const { getByLabelText } = render(
+    const { getByLabelText, getByRole, getByPlaceholderText } = render(
       <MemoryRouter>
         <CategoryForm
           handleSubmit={handleSubmitMock}
-          value="Books"
+          value="Cats"
           setValue={setValueMock}
         />
       </MemoryRouter>
     );
 
-    const form = getByLabelText("category-form");
-    fireEvent.submit(form);
+    const input = getByPlaceholderText("Enter new category");
+    const submitButton = getByRole("button", { name: /submit/i });
+    expect(submitButton).not.toBeDisabled();
 
-    expect(handleSubmitMock).toHaveBeenCalled();
-    expect(setValueMock).toHaveBeenCalledWith(""); // Expect input to be cleared after submission
+    fireEvent.submit(input);
+
+    expect(handleSubmitMock).toHaveBeenCalledTimes(1);
+    expect(setValueMock).toHaveBeenCalledWith("");
   });
 
   it("disables submit button when input is empty", () => {
@@ -87,34 +87,6 @@ describe("CategoryForm Component", () => {
 
     const submitButton = getByRole("button", { name: /submit/i });
     expect(submitButton).toBeDisabled();
-  });
-
-  it("clears input field after submission", () => {
-    const MockComponent = () => {
-      const [value, setValue] = useState("");
-      const handleSubmit = jest.fn();
-
-      return (
-        <CategoryForm
-          handleSubmit={handleSubmit}
-          value={value}
-          setValue={setValue}
-        />
-      );
-    };
-
-    const { getByPlaceholderText, getByText } = render(<MockComponent />);
-
-    const input = getByPlaceholderText("Enter new category");
-    const submitButton = getByText("Submit");
-
-    fireEvent.change(input, { target: { value: "New Category" } });
-
-    expect(input.value).toBe("New Category");
-
-    fireEvent.click(submitButton);
-
-    expect(input.value).toBe("");
   });
 
   it("prevents submission when input is empty", () => {
@@ -134,11 +106,10 @@ describe("CategoryForm Component", () => {
     const input = getByPlaceholderText("Enter new category");
     const submitButton = getByRole("button", { name: /submit/i });
 
-    // Submit button should be disabled initially
     expect(submitButton).toBeDisabled();
 
     fireEvent.change(input, { target: { value: "   " } });
-    expect(submitButton).toBeDisabled(); // Submit button should still be disabled
+    expect(submitButton).toBeDisabled();
 
     fireEvent.click(submitButton);
 
