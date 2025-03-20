35ca1707a8c8fb4632adf949d09aa7216cd3f476
cetigerlily <celestigerlily@gmail.com>
2025-03-20 23:20:19 +0800

    fix: fix original ui testing for search flow

--- tests/search.spec.cjs
+++ tests/search.spec.cjs
@@ -5,7 +5,7 @@ const mockSearchResults = [
   {
     _id: 1,
     name: "Textbook",
-    slug: "Textbook",
+    slug: "textbook",
     description: "A comprehensive textbook",
     price: 79.99,
     category: "book",
@@ -15,7 +15,7 @@ const mockSearchResults = [
   {
     _id: 2,
     name: "The Law of Contract in Singapore",
-    slug: "The-Law-of-Contract-in-Singapore",
+    slug: "the-law-of-contract-in-singapore",
     description: "A bestselling book in Singapore",
     price: 54.99,
     category: "book",
@@ -77,19 +77,42 @@ test.describe("Search for a product", () => {
     const resultsCount = page.getByRole("heading", { name: "Found 2" });
     await expect(resultsCount).toBeVisible();
 
-    mockSearchResults.map(async (searchResult) => {
-      const name = page.getByRole("heading", { name: searchResult.name })
-      const description = page.getByText(`${ searchResult.description.substring(0, 30) }...`);
-      const price = page.getByText(`$${ searchResult.price }`);
-      const image = page.getByTestId(`s-img-${ searchResult.name }`);
-      const button = page.getByTestId(`s-md-button-${ searchResult.slug }`);
+    const title_1 = page.getByRole('heading', { name: 'The Law of Contract in' });
+    const title_2 = page.getByRole('heading', { name: 'Textbook' });
+    const description_1 = page.getByText('A bestselling book in');
+    const description_2 = page.getByText('A comprehensive textbook...');
+    const price_1 = page.getByText('$79.99');
+    const price_2 = page.getByText('$54.99');
+    const image_1 = page.getByTestId('s-img-The Law of Contract in Singapore');
+    const image_2 = page.getByTestId('s-img-Textbook');
+    const button_1 = page.getByTestId('s-md-button-textbook');
+    const button_2 = page.getByTestId('s-md-button-the-law-of-contract-in-singapore');
 
-      await expect(name).toBeVisible();
-      await expect(description).toBeVisible();
-      await expect(price).toBeVisible();
-      await expect(image).toBeVisible();
-      await expect(button).toBeVisible();
-    });
+    await expect(title_1).toBeVisible();
+    await expect(title_2).toBeVisible();
+    await expect(description_1).toBeVisible();
+    await expect(description_2).toBeVisible();
+    await expect(price_1).toBeVisible();
+    await expect(price_2).toBeVisible();
+    await expect(image_1).toBeVisible();
+    await expect(image_2).toBeVisible();
+    await expect(button_1).toBeVisible();
+    await expect(button_2).toBeVisible();
+
+    // NOTE: Tests would not pass for original method because title which was rendered did not always match the full one due to scope of view
+    // mockSearchResults.map(async (searchResult) => {
+    //   const name = page.getByRole("heading", { name: searchResult.name })
+    //   const description = page.getByText(`${ searchResult.description.substring(0, 30) }...`);
+    //   const price = page.getByText(`$${ searchResult.price }`);
+    //   const image = page.getByTestId(`s-img-${ searchResult.name }`);
+    //   const button = page.getByTestId(`s-md-button-${ searchResult.slug }`);
+    //
+    //   await expect(name).toBeVisible();
+    //   await expect(description).toBeVisible();
+    //   await expect(price).toBeVisible();
+    //   await expect(image).toBeVisible();
+    //   await expect(button).toBeVisible();
+    // });
   });
 
   test("should display message when products don't exist for search query", async ({ page }) => {
