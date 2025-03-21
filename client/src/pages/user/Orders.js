4ee8de8de4176a88b4b419437c6b6e163d161a78
Kalaris Ng <kalaris3255@gmail.com>
2025-03-21 22:07:48 +0800

    fix: restore orders

--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -30,60 +30,56 @@ const Orders = () => {
           </div>
           <div className="col-md-9">
             <h1 className="text-center">All Orders</h1>
-            {orders.length === 0 ? (
-              <p>No orders found</p> // Or any other message
-            ) : (
-              orders?.map((o, i) => {
-                return (
-                  <div className="border shadow" key={o._id}>
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th scope="col">#</th>
-                          <th scope="col">Status</th>
-                          <th scope="col">Buyer</th>
-                          <th scope="col">Date</th>
-                          <th scope="col">Payment</th>
-                          <th scope="col">Quantity</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>{i + 1}</td>
-                          <td>{o?.status}</td>
-                          <td>{o?.buyer?.name}</td>
-                          <td>{moment(o?.createAt).fromNow()}</td>
-                          <td data-testid="payment-status">
-                            {o?.payment.success ? "Success" : "Failed"}
-                          </td>
-                          <td data-testid="quantity">{o?.products?.length}</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                    <div className="container">
-                      {o?.products?.map((p, i) => (
-                        <div className="row mb-2 p-3 card flex-row" key={p._id}>
-                          <div className="col-md-4">
-                            <img
-                              src={`/api/v1/product/product-photo/${p._id}`}
-                              className="card-img-top"
-                              alt={p.name}
-                              width="100px"
-                              height={"100px"}
-                            />
-                          </div>
-                          <div className="col-md-8">
-                            <p>{p.name}</p>
-                            <p>{p.description.substring(0, 30)}</p>
-                            <p>Price : {p.price}</p>
-                          </div>
+            {orders?.map((o, i) => {
+              return (
+                <div className="border shadow">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Buyer</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Payment</th>
+                        <th scope="col">Quantity</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{i + 1}</td>
+                        <td>{o?.status}</td>
+                        <td>{o?.buyer?.name}</td>
+                        <td>{moment(o?.createAt).fromNow()}</td>
+                        <td data-testid="payment-status">
+                          {o?.payment.success ? "Success" : "Failed"}
+                        </td>
+                        <td data-testid="quantity">{o?.products?.length}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="container">
+                    {o?.products?.map((p, i) => (
+                      <div className="row mb-2 p-3 card flex-row" key={p._id}>
+                        <div className="col-md-4">
+                          <img
+                            src={`/api/v1/product/product-photo/${p._id}`}
+                            className="card-img-top"
+                            alt={p.name}
+                            width="100px"
+                            height={"100px"}
+                          />
                         </div>
-                      ))}
-                    </div>
+                        <div className="col-md-8">
+                          <p>{p.name}</p>
+                          <p>{p.description.substring(0, 30)}</p>
+                          <p>Price : {p.price}</p>
+                        </div>
+                      </div>
+                    ))}
                   </div>
-                );
-              })
-            )}
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
