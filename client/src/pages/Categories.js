857f724b182a15df2c76a48071d4b2bb54604872
Jishnu <e0725046@u.nus.edu>
2025-03-23 18:54:25 +0800

    feat: add UI tests for categories page

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -1,15 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import useCategory from "../hooks/useCategory";
+import axios from "axios";
 import Layout from "../components/Layout";
+
+
 const Categories = () => {
-  const categories = useCategory();
+  const [categories, setCategories] = useState([]);
+
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data?.success) {
+        setCategories(data.category);
+      } else {
+        console.error("API response does not contain expected data")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+
   return (
     <Layout title={"All Categories"}>
       <div className="container">
-        <div className="row">
+        <div className="row mt-4">
+          <h2>All Categories</h2>
+          <p className="instruction">Click on a category below to see the items in that category</p>
           {categories.map((c) => (
-            <div className="col-md-6 mt-5 mb-3 gx-3 gy-3" key={c._id}>
+            <div className="row-md-6 mt-2 mb-3 gx-3 gy-3" key={c._id}>
               <Link to={`/category/${c.slug}`} className="btn btn-primary">
                 {c.name}
               </Link>
